6916630e008fe439ba094ec70c77a346eef6c0aa
Nithinreddy999 <nithinreddy2001@gmail.com>
2022-08-08 20:41:45 +0530

    Fifth Problem(Assignment)

--- Demo.java
+++ Demo.java
@@ -1,5 +1,29 @@
 public class Demo {
     public static void main(String args[]){
-        System.out.println("nithin");
+        System.out.println("Nithin");
+        int id=1;
+        switch(id){
+            case 1:
+                System.out.println("id is positive");
+            case 2:
+                System.out.println("id is negative");
+            case 3:
+                System.out.println("id is negative");
+        }
     }
 }
+    /*public class DemoKeywords{
+        int id=1;
+        int max=0;
+       /* DemoKeywords(int id){
+            this.id=id;
+        }
+    }
+
+    while(id<=1){
+        max++;
+    }
+    do{
+        max++;
+        }while(id<=1);
+    }*/
--- Second.java
+++ Second.java
@@ -3,72 +3,82 @@ public class Second {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int Mon,Tue,Wed,Thr,Fri,Sat,Sun;
+        int Mon=0;
+                int Tue = 0;
+                int Wed=0;
+                int Thr=0;
+                int Fri=0;
+                int Sat=0;
+                int Sun=0;
         int Bonus,Brate;
         int a[]=new int[7];
-        for(int i=1;i<=7;i++)
+        for(int i=0;i<a.length;i++)
         {
             a[i]=sc.nextInt();
         }
-        Sun = a[1]*150;
-        Sat = a[7]*125;
+        Sun = a[0]*150;
+        Sat = a[6]*125;
+        if(a[1]<=8)
+        {
+            Mon = a[1]*8;
+        }
+        else if(a[1]>8)
+        {
+            Bonus = a[1]-8;
+            Brate = Bonus * 115;
+            Mon = Brate + 800;
+        }
+
         if(a[2]<=8)
         {
-            Mon = a[2]*8;
+            Tue = a[2]*8;
         }
         else if(a[2]>8)
         {
             Bonus = a[2]-8;
             Brate = Bonus * 115;
-            Mon = Brate + 800;
+            Tue = Brate + 800;
         }
 
         if(a[3]<=8)
         {
-            Tue = a[2]*8;
+            Wed = a[3]*8;
         }
         else if(a[3]>8)
         {
             Bonus = a[3]-8;
             Brate = Bonus * 115;
-            Tue = Brate + 800;
+            Wed = Brate + 800;
         }
 
         if(a[4]<=8)
         {
-            Wed = a[4]*8;
+            Thr = a[4]*8;
         }
         else if(a[4]>8)
         {
             Bonus = a[4]-8;
             Brate = Bonus * 115;
-            Wed = Brate + 800;
+            Thr = Brate + 800;
         }
 
         if(a[5]<=8)
         {
-            Thr = a[5]*8;
+            Fri = a[5]*8;
         }
         else if(a[5]>8)
         {
             Bonus = a[5]-8;
             Brate = Bonus * 115;
-            Thr = Brate + 800;
-        }
-
-        if(a[6]<=8)
-        {
-            Fri = a[6]*8;
-        }
-        else if(a[6]>8)
-        {
-            Bonus = a[6]-8;
-            Brate = Bonus * 115;
             Fri = Brate + 800;
         }
 
-        int total,extradays,extrarate,totalsalary;
-        total = a[2]+a[3]+a[4]+a[5]+a[6];
+        int total=0;
+        int extradays=0;
+        int extrarate=0;
+        int totalsalary=0;
+        total = a[1]+a[2]+a[3]+a[4]+a[5]
+        ;
         if(total>40)
         {
             extradays = total-40;
