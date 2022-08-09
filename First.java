9e758aad68fa00e224ad44d98b614f40695d36cf
Nithinreddy999 <nithinreddy2001@gmail.com>
2022-08-09 11:34:53 +0530

    Day2 - First Problem

---
+++ First.java
@@ -0,0 +1,16 @@
+package Day2;
+import java.util.*;
+public class First {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.println("Minimum Marks:"+a[0]);
+        System.out.print("Maximum Marks:"+a[n-1]);
+    }
+}
